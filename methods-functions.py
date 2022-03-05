37bb4e6c97a2a02ac7d223c48178e013e3b375e5
CodeBlueOcean <95150255+CodeBlueOcean@users.noreply.github.com>
2022-03-04 20:17:02 -0600

    Add files via upload

---
+++ methods-functions.py
@@ -0,0 +1,15 @@
+# Methods vs functions
+# list()
+# print()
+# max()
+# min()
+# input()
+
+def some_random_stuff():
+    pass
+
+some_random_stuff()
+
+# a method has to be own by something to the left . 
+
+'hello'.capitalize()
